c0f658599981c91ab853d0937e1c7184c9740a2c
alex <alexmiller.ml@gmail.com>
2020-08-29 10:54:33 -0400

    add thailand 2

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -15,7 +15,7 @@ import { Viewer } from '../viewer/viewer';
 export function App() {
   const [content, setContent] = useState('');
 
-  const countries = ['Greece', 'India'];
+  const countries = ['Greece', 'India', 'Thailand'];
 
   return (
     <div>
